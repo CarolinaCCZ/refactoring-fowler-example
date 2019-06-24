969bf7e550452b6fd6a2e85878f3d6c02d271845
CarolinaCCZ <ccz0001@alu.ubu.es>
2019-06-24 19:59:59 +0200

    Renombro each por aRental

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -56,12 +56,12 @@ public class Customer {
 		return result;
 	}
 
-	private int getFrequentRentalPoints(Rental each) {
+	private int getFrequentRentalPoints(Rental aRental) {
 		int frequentRenterPoints = 0;
 		frequentRenterPoints ++;
 		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
+		if ((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& aRental.getDaysRented() > 1)
 			frequentRenterPoints++;
 		return frequentRenterPoints;
 	}
